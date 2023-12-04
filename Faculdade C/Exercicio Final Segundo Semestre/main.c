e85119be6971233b5d9fbb8d07cf6c5cfa1e37f6
Pedro Henrique Faria <132838088+PhTheDev@users.noreply.github.com>
2023-12-04 09:49:55 -0300

    Update main.c

--- Faculdade C/Exercicio Final Segundo Semestre/main.c
+++ Faculdade C/Exercicio Final Segundo Semestre/main.c
@@ -11,7 +11,35 @@ int main(){
         printf("Falha na abertura de arquivo!");
         exit(1);
     }
-    menuCliente();
+    short opcaomenu;
+
+    do {
+        printf("\n\n------------------------MENU-PRINCIPAL------------------------\n");
+        printf("\n1 - Menu Cliente\n");
+        printf("2 - Menu Produto\n");
+        printf("3 - Menu Pedido\n");
+        printf("4 - Sair do programa\n\n");
+        printf("-------------------------------------------------------------\n");
+        printf("\n\nDigite a opcao desejada: ");
+        scanf("%hd", &opcaomenu);  // Use %hd para short
+        switch (opcaomenu) {
+            case 1:
+                opcaomenu = menuCliente(); // Chama o menu do cliente
+                break;
+            case 2:
+                //opcaomenu = menuProduto();
+                break;
+            case 3:
+                //opcaomenu = menuPedido();
+                break;    
+            case 4:
+                printf("Saindo...\n");
+                break;
+            default:
+                printf("Opcao invalida tente novamente!");
+                break;
+        }
+    } while (opcaomenu != 4);
 
     return 0;
 }    
